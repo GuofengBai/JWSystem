7288a8f15dfbc48f452078caf00c10063802ff00
Administrator <georgecurz@qq.com>
2017-06-12 22:01:18 +0800

    登录页、学生主页、登录Controller（假的）

---
+++ src/main/java/edu/nju/vo/onlineUserVO.java
@@ -0,0 +1,20 @@
+package edu.nju.vo;
+
+/**
+ * Created by Administrator on 2017/6/12.
+ */
+public class onlineUserVO {
+    String username;
+
+    public onlineUserVO(String username) {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
---
+++ src/main/java/edu/nju/vo/userVO.java
@@ -0,0 +1,28 @@
+package edu.nju.vo;
+
+/**
+ * Created by Administrator on 2017/6/12.
+ */
+public class userVO {
+    private String userName;
+    private String password;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public userVO() {
+    }
+}
